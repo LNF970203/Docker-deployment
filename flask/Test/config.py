826d8d4b39473b1a87a343648f772c486bff6f47
LNF970203 <lachinnanu123@gmail.com>
2024-07-14 11:45:33 +0530

    update(configurations):add hosted IP address and changed service name

--- flask/Test/config.py
+++ flask/Test/config.py
@@ -1,1 +1,3 @@
 ENDPOINT_URL = "http://localhost:5000/get-name"
+
+HOSTED_IP = "http://54.175.120.53:5000/get-name"
--- flask/Test/test.py
+++ flask/Test/test.py
@@ -5,7 +5,7 @@ import json
 import config
 
 
-def convert_base64():
+def create_payload():
     
     payload_structure = {
         "body": {
@@ -16,11 +16,11 @@ def convert_base64():
     return payload_structure
     
 
-def get_features(path: str, url: str = config.ENDPOINT_URL):
+def get_features(url: str = config.HOSTED_IP):
     """
     send a post REST api request to flask backend
     """
-    image_payload = convert_base64()
+    image_payload = create_payload()
     headers = {'Content-Type': 'application/json'}
     endpoint_response = requests.post(url, data = json.dumps(image_payload), headers = headers)
     response = getattr(endpoint_response, '_content').decode("utf-8")
@@ -30,6 +30,5 @@ def get_features(path: str, url: str = config.ENDPOINT_URL):
 
 
 if __name__ == "__main__":
-    image_path = "test_image.jpg"
-    response = get_features(image_path)
+    response = get_features()
     print(response)
