444f9d918ef0ce947bb57b13225039f88dae05d4
LNF970203 <lachinnanu123@gmail.com>
2024-07-14 09:39:50 +0530

    update(files):necessary update for aws deployment

--- flask/Test/config.py
+++ flask/Test/config.py
@@ -1,1 +1,3 @@
 ENDPOINT_URL = "http://localhost:5000/get-name"
+
+HOSTED_URL = "http://18.212.216.225:5000/get-name"
--- flask/Test/test.py
+++ flask/Test/test.py
@@ -5,7 +5,7 @@ import json
 import config
 
 
-def convert_base64():
+def create_payload():
     
     payload_structure = {
         "body": {
@@ -16,11 +16,11 @@ def convert_base64():
     return payload_structure
     
 
-def get_features(path: str, url: str = config.ENDPOINT_URL):
+def get_features(path: str, url: str = config.HOSTED_URL):
     """
     send a post REST api request to flask backend
     """
-    image_payload = convert_base64()
+    image_payload = create_payload()
     headers = {'Content-Type': 'application/json'}
     endpoint_response = requests.post(url, data = json.dumps(image_payload), headers = headers)
     response = getattr(endpoint_response, '_content').decode("utf-8")
